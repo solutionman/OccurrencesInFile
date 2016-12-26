43f8a2902757d8e0a62f13a27f7634667f266030
solutionman <solutionman@inbox.ru>
2016-12-26 16:49:16 +0700

    name of file and check

--- SearchInFile/src/Search.java
+++ SearchInFile/src/Search.java
@@ -1,10 +1,14 @@
 // searching occurrences in file (mostly in xls)
 
+import java.util.Scanner;
+
 public class Search {
     public static void main(String[] args){
-
-
+        Scanner userInput = new Scanner(System.in);
         System.out.println("Enter name of file");
 
+        String nameOfFile = userInput.nextLine();
+
+        System.out.println("Your file is: " + nameOfFile);
     }
 }
