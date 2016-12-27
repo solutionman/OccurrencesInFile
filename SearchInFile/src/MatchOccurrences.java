375b26d946c5288be5978b6e095f6019a6a94855
solutionman <solutionman@inbox.ru>
2016-12-27 18:57:21 +0700

    works with 2 numbers

--- SearchInFile/src/MatchOccurrences.java
+++ SearchInFile/src/MatchOccurrences.java
@@ -4,12 +4,13 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.DoubleSummaryStatistics;
 
 public class MatchOccurrences {
     private String nameOfFile;
     private String numberToSearch;
 
-    public MatchOccurrences(String nameOfFile, String numberToSearch){
+    public MatchOccurrences(String nameOfFile, String numberToSearch) {
         this.nameOfFile = nameOfFile;
         this.numberToSearch = numberToSearch;
 
@@ -17,7 +18,7 @@ public class MatchOccurrences {
     }
 
 
-    public int catchOccurrences(){
+    public int catchOccurrences() {
 
         String line = "";
         String unparsedFile = "";
@@ -27,22 +28,40 @@ public class MatchOccurrences {
             FileReader reader = new FileReader(nameOfFile);
             BufferedReader buffReader = new BufferedReader(reader);
 
-            while((line = buffReader.readLine()) != null){
+            while ((line = buffReader.readLine()) != null) {
                 unparsedFile += line;
             }
             buffReader.close();
 
             arrayOfLines = unparsedFile.split("\"");
 
-            for(int i=0; i< arrayOfLines.length; ++i){
-                if(arrayOfLines[i].equals(numberToSearch) ){
-                    System.out.println("Catch " + arrayOfLines[i]);
+            for(int i=0; i < arrayOfLines.length; ++i){
+                try{
+                    double temp = Double.parseDouble(arrayOfLines[i]);
+                }catch (Exception e){
+                    arrayOfLines[i] = "0";
                 }
-                //System.out.println("get something");
+            }
+
+            for (int i = 0; i < arrayOfLines.length; ++i) {
+                for(int j = i; j< arrayOfLines.length; ++j){
+
+                    if (arrayOfLines[i].equals(numberToSearch)) {
+                        System.out.println("Catch " + arrayOfLines[i]);
+                    }
+
+                    double temp = Double.parseDouble(arrayOfLines[i]) + Double.parseDouble(arrayOfLines[j]);
+                    String tempString = String.valueOf(temp);
+
+                    if (tempString.equals(numberToSearch)){
+                        System.out.printf("Catch %s + %s = %s\n", arrayOfLines[i], arrayOfLines[j], numberToSearch);
+                    }
+                    //System.out.println("get something");
+                }
             }
 
             return 2;
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Something goes wrong during reading file" + e);
         }
 
