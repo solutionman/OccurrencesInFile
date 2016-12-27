6ab3735a9bbf09158e61de912070b3c4ae49d82c
solutionman <solutionman@inbox.ru>
2016-12-27 15:43:45 +0700

    read file (not complete)

--- SearchInFile/src/MatchOccurrences.java
+++ SearchInFile/src/MatchOccurrences.java
@@ -1,6 +1,10 @@
 
 // Here we will compare numbers
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MatchOccurrences {
     private String nameOfFile;
     private double numberToSearch;
@@ -14,7 +18,19 @@ public class MatchOccurrences {
 
 
     public int catchOccurrences(){
-        return 2;
+        try {
+            FileReader reader = new FileReader(nameOfFile);
+            BufferedReader buffReader = new BufferedReader(reader);
+
+
+
+            buffReader.close();
+            return 2;
+        }catch (IOException e){
+            System.out.println("Something goes wrong during reading file" + e);
+        }
+
+        return -1;
     }
 
 }
