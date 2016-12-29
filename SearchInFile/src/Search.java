9416ff9fa130268d6495f4ddbfd495647f427967
solutionman <solutionman@inbox.ru>
2016-12-29 16:17:34 +0700

    class for writing results to file (empty for a while)

--- SearchInFile/src/Search.java
+++ SearchInFile/src/Search.java
@@ -28,6 +28,7 @@ public class Search {
 
         reestr.catchOccurrences();
 
+
     }
 }
 
---
+++ SearchInFile/src/WriteToFile.java
@@ -0,0 +1,7 @@
+// Here we write possible occurrences to file  (txt)
+
+public class WriteToFile {
+    public void writeToFileResults(){
+
+    }
+}
